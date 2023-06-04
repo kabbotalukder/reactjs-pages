ce3e2e8cf1d8303be665d0d0c63755e48b929a43
Kabbo Talukder <kabbotalukder5@gmail.com>
2023-06-04 20:53:36 +0600

    Updates

---
+++ precache-manifest.bcc51685822f188c80c8ae44efc773b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e143741f0fe6c6d35d8",
+    "url": "/reactjs-pages/static/css/main.2bdd5f09.chunk.css"
+  },
+  {
+    "revision": "0e143741f0fe6c6d35d8",
+    "url": "/reactjs-pages/static/js/main.0e143741.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/reactjs-pages/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "0365d3d0799053e796be",
+    "url": "/reactjs-pages/static/js/2.0365d3d0.chunk.js"
+  },
+  {
+    "revision": "24cbe5e0692db2d9c5b4",
+    "url": "/reactjs-pages/static/js/runtime~main.24cbe5e0.js"
+  },
+  {
+    "revision": "0d2e26414fdca3ff4a0590f699c241e4",
+    "url": "/reactjs-pages/index.html"
+  }
+];
