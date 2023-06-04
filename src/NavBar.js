82fc319ba19254d23e54707ef27be32d8b5451e7
Kabbo Talukder <kabbotalukder5@gmail.com>
2023-06-04 19:57:15 +0600

    Initialize project using Create React App

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>home</li>
+            </ul>
+            <ul>
+                <li>about</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/UnderConst.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UnderConst = () => {
+    return(
+        <h2>This Page is Under Construction</h2>
+    );
+}
+
+export default UnderConst;
